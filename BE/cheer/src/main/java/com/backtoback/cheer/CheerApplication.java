f7e7822878cb94eae64a3a987ef022421b5c08de
yurim <yurimkang20@gmail.com>
2023-05-10 15:22:27 +0900

    init: 응원 프로젝트 구성

---
+++ BE/cheer/src/main/java/com/backtoback/cheer/CheerApplication.java
@@ -0,0 +1,13 @@
+package com.backtoback.cheer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CheerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CheerApplication.class, args);
+	}
+
+}
