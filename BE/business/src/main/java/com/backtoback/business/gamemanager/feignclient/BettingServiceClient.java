51e9a71a9e70c5079b363f0f7661f35fbe0e0250
유환규 <hwanguebest@naver.com>
2023-05-16 03:14:32 +0900

    feat: business client 기능 추가
    - feign client 추가

--- BE/business/src/main/java/com/backtoback/business/gamemanager/feignclient/BettingServiceClient.java
+++ BE/business/src/main/java/com/backtoback/business/gamemanager/feignclient/BettingServiceClient.java
@@ -2,12 +2,13 @@ package com.backtoback.business.gamemanager.feignclient;
 
 
 import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
-@FeignClient(name = "betting-service", url = "http://localhost:8084")
+@FeignClient(name = "betting-service", url = "http://k8a708.p.ssafy.io:8084")
 public interface BettingServiceClient {
 
-  @GetMapping("/makeRoom")
-  void makeRoom();
-
+  @PostMapping("/api/betting/start")
+  public ResponseEntity<?> readyToStartBetting();
 }
