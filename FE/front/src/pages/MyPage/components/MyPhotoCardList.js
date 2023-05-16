5fda197ed99f972f30fde8e7e340a92ad673bfb3
heeralce <heeracle@naver.com>
2023-05-17 02:13:32 +0900

    feat: 마이페이지 틀

---
+++ FE/front/src/pages/MyPage/components/MyPhotoCardList.js
@@ -0,0 +1,11 @@
+import "./styles/MyPhotoCardList.css";
+
+const MyPhotoCardList = () => {
+  return (
+    <div>
+      <div className="page-title">My Photo Card</div>
+    </div>
+  );
+};
+
+export default MyPhotoCardList;
