c880f2747b8f218fed14d2e23df2f830cd362496
yurim <yurimkang20@gmail.com>
2023-05-16 22:03:38 +0900

    feat: Game 정보 조회 구현

--- FE/front/src/pages/GameRoom/GameRoom.js
+++ FE/front/src/pages/GameRoom/GameRoom.js
@@ -2,8 +2,45 @@ import Betting from "../Betting";
 import Cheer from "../Cheer";
 import Chat from "../Chat";
 import "./GameRoom.css";
+import { useEffect , useState} from "react";
+import axios from "axios";
+import { useParams } from "react-router-dom";
 
 const GameRoom = () => {
+
+  // const {gameSeq} =  useParams();
+  const gameSeq = 3;
+
+  const [homeSeq, setHomeSeq] = useState(0);
+  const [awaySeq, setAwaySeq] = useState(0);
+  const [homeName, setHomeName] = useState(null);
+  const [awayName, setAwayName] = useState(null);
+  const [gameState, setGameState] = useState(null);
+  const [topicNumber, setTopicNumber] = useState(0);
+
+  const getGameInfo = async () => {
+    try { // 게임 정보 받아오기
+      const response = await axios.get('http://localhost:8000/api/business/games/' + gameSeq + '/info')
+      .then(response => {
+        console.log(response.data);
+        setHomeSeq(response.data.homeSeq);
+        setAwaySeq(response.data.awaySeq);
+        setHomeName(response.data.homeName);
+        setAwayName(response.data.awayName);
+        setGameState(response.data.gameActiveType);
+        setTopicNumber(response.data.topicNumber);
+      });
+      console.log("GameRoom Info Success..............");
+    } catch (error) {
+      console.log("Game Info Error....");
+    }
+  }
+
+  useEffect(() => {
+    console.log("GameRoom render..............");
+    getGameInfo();
+  }, []);
+
   return (
     <div class="grid gameroom-container m-3">
       <div class="col-7 left">
@@ -13,17 +50,18 @@ const GameRoom = () => {
           //style해제하고 스트리밍 컴포넌트 넣으면 됩니다.
         ></div>
         <div className="cheer">
-          <Cheer {...{ gameSeq: 4, homeSeq: 3, awaySeq: 2 }} />
+          <Cheer {...{ gameSeq: gameSeq, homeSeq: homeSeq, awaySeq: awaySeq }} />
         </div>
+        <div><h2>{gameState}</h2></div>
         <div className="betting">
           <Betting
             {...{
-              gameSeq: 4,
-              homeSeq: 3,
-              awaySeq: 2,
-              homeName: "LG",
-              awayName: "키움",
-              gameActive: true,
+              gameSeq: gameSeq,
+              homeSeq: homeSeq,
+              awaySeq: awaySeq,
+              homeName: homeName,
+              awayName: awayName,
+              gameActive: gameState,
             }}
           />
         </div>
