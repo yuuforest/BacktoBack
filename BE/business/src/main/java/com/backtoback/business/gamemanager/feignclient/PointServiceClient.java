500cd2e38807942ff25bf9d7384986f7b88d0d66
유환규 <hwanguebest@naver.com>
2023-05-16 10:07:43 +0900

    feat: pointservice client 기능 추가
    - feign client 추가
    - application.yml 설정

--- BE/business/src/main/java/com/backtoback/business/gamemanager/feignclient/PointServiceClient.java
+++ BE/business/src/main/java/com/backtoback/business/gamemanager/feignclient/PointServiceClient.java
@@ -3,11 +3,10 @@ package com.backtoback.business.gamemanager.feignclient;
 
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 
-@FeignClient(name = "betting-service", url = "http://k8a708.p.ssafy.io:8084")
-public interface BettingServiceClient {
+@FeignClient(name = "point-service", url = "http://k8a708.p.ssafy.io:8084")
+public interface PointServiceClient {
 
   @PostMapping("/api/betting/start")
   public ResponseEntity<?> readyToStartBetting();
