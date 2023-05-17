d1480b8b583fa001a8a3a062cc409f45a417cd5c
yurim <yurimkang20@gmail.com>
2023-05-17 09:25:22 +0900

    feat: pointLog 조회 기능 구현
    point, cheer 프로젝트 서버와 연결 확인 필요 + 서버 Database와 연결 확인 필요

--- BE/point/src/main/java/com/backtoback/point/pointlog/repository/PointLogRepository.java
+++ BE/point/src/main/java/com/backtoback/point/pointlog/repository/PointLogRepository.java
@@ -4,6 +4,11 @@ import com.backtoback.point.pointlog.domain.PointLog;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface PointLogRepository extends JpaRepository<PointLog, Long> {
+
+    List<PointLog> findByMemberMemberSeqOrderByPointLogSeqDesc(Long memberSeq);
+
 }
