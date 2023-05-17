87c1c350d390dbbafa083b88ba9c9dce81fca6db
유환규 <hwanguebest@naver.com>
2023-05-17 13:55:37 +0900

    fix: Update MessageDto.java

--- BE/media/src/main/java/com/backtoback/media/video/dto/MessageDto.java
+++ BE/media/src/main/java/com/backtoback/media/video/dto/MessageDto.java
@@ -14,18 +14,13 @@ public class MessageDto implements Serializable {
 
     private GameActiveType gameActiveType;
 
-<<<<<<< HEAD
+
     private Long mediaStartTime;
 
     public MessageDto(Long gameSeq, GameActiveType gameActiveType,Long mediaStartTime) {
         this.gameSeq = gameSeq;
         this.gameActiveType = gameActiveType;
         this.mediaStartTime = mediaStartTime;
-=======
-    public MessageDto(Long gameSeq, GameActiveType gameActiveType) {
-        this.gameSeq = gameSeq;
-        this.gameActiveType = gameActiveType;
->>>>>>> d3e242c9852359e36e08f18e14d7f969c3bdb292
     }
 
     @Override
