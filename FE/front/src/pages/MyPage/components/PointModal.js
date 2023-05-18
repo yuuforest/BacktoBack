704b15eafbb1c82c679c0ef20d3d793e4760f0c6
heeralce <heeracle@naver.com>
2023-05-18 21:52:13 +0900

    프로필 디자인 수정

--- FE/front/src/pages/MyPage/components/PointModal.js
+++ FE/front/src/pages/MyPage/components/PointModal.js
@@ -1,54 +1,68 @@
 import React, { useEffect, useState } from "react";
-import { Button } from 'primereact/button';
-import { Dialog } from 'primereact/dialog';
+import { Button } from "primereact/button";
+import { Dialog } from "primereact/dialog";
 import { DataTable } from "primereact/datatable";
 import { Column } from "primereact/column";
 import axios from "axios";
+import "./styles/PointModal.css";
 
 import { useRecoilValue } from "recoil";
 import { memberSeq } from "../../../components/State/UserState";
 
 function PointModal() {
+  /* 멤버 정보 */
+  const memSeq = useRecoilValue(memberSeq);
 
-    /* 멤버 정보 */
-    const memSeq = useRecoilValue(memberSeq);
+  const [visible, setVisible] = useState(false);
+  const [infos, setInfos] = useState([]);
 
-    const [visible, setVisible] = useState(false);
-    const [infos, setInfos] = useState([]);
-
-    const getPoingLogs = async () => {
-        console.log("Point Log method....");
-        try{
-            await axios
-                .get("http://k8a708.p.ssafy.io/api/point/point-log/member/" + memSeq)
-                .then((response) => {
-                    // console.log(response);
-                    // console.log(response.data);
-                    setInfos(response.data);
-                })
-        } catch (error) {
-            console.log("Point Log Error....");
-        }
+  const getPoingLogs = async () => {
+    console.log("Point Log method....");
+    try {
+      await axios
+        .get("http://k8a708.p.ssafy.io/api/point/point-log/member/" + memSeq)
+        .then((response) => {
+          // console.log(response);
+          // console.log(response.data);
+          setInfos(response.data);
+        });
+    } catch (error) {
+      console.log("Point Log Error....");
     }
+  };
 
-    useEffect(() => {
-        getPoingLogs();
-    }, []);
+  useEffect(() => {
+    getPoingLogs();
+  }, []);
 
-    return (
-        <div className="card flex justify-content-center">
-            <Button label="포인트 사용 내역 보러가기" icon="pi pi-search" severity="success" onClick={() => setVisible(true)} outlined />
-            {/* <Button label="포인트 이력" icon="pi pi-external-link" onClick={() => setVisible(true)} /> */}
-            <Dialog header="포인트 내역" visible={visible} style={{ width: '50vw' }} onHide={() => setVisible(false)}>
-                <DataTable value={infos}>
-                    <Column field="num" header="번호"></Column>
-                    <Column field="point" header="포인트"></Column>
-                    <Column field="detail" header="상세 내역 "></Column>
-                    <Column field="time" header="날짜"></Column>
-                </DataTable>
-            </Dialog>
-        </div>
-    )
+  return (
+    <div className="flex justify-content-end">
+      <div style={{ textAlign: "right" }}>
+        <Button
+          className="point-button"
+          label="포인트 사용 내역 보러가기"
+          icon="pi pi-search"
+          //   severity="success"
+          onClick={() => setVisible(true)}
+          outlined
+        />
+      </div>
+      {/* <Button label="포인트 이력" icon="pi pi-external-link" onClick={() => setVisible(true)} /> */}
+      <Dialog
+        header="포인트 내역"
+        visible={visible}
+        style={{ width: "50vw" }}
+        onHide={() => setVisible(false)}
+      >
+        <DataTable value={infos}>
+          <Column field="num" header="번호"></Column>
+          <Column field="point" header="포인트"></Column>
+          <Column field="detail" header="상세 내역 "></Column>
+          <Column field="time" header="날짜"></Column>
+        </DataTable>
+      </Dialog>
+    </div>
+  );
 }
 
 export default PointModal;
--- FE/front/src/pages/MyPage/components/Profile.js
+++ FE/front/src/pages/MyPage/components/Profile.js
@@ -50,39 +50,40 @@ const Profile = ({ cards }) => {
   }, [cards]);
 
   return (
-    <div className="grid profile-container mt-3 mb-3">
-      <div className="col-5 profile-left grid">
-        <div className="my-team-img col-5">
-          <img
-            alt="teamImage"
-            src={imageUrl}
-            style={{ width: "100%", height: "auto" }}
-          ></img>
+    <div>
+      <div className="grid profile-container mt-3 mb-3">
+        <div className="col-5 profile-left grid">
+          <div className="my-team-img col-5">
+            <img
+              alt="teamImage"
+              src={imageUrl}
+              style={{ width: "100%", height: "auto" }}
+            ></img>
+          </div>
+          <div className="col-6">
+            <div className="nickname">{curNickname}</div>
+            {/* <i className="pi pi-cog" style={{ fontSize: "2rem" }}></i> */}
+            <Button
+              className="custom-button"
+              icon="pi pi-cog"
+              onClick={handleUpdateModal}
+            ></Button>
+            <MemberUpdateModal
+              showModal={showModal}
+              closeModal={handleUpdateModal}
+            />
+          </div>
         </div>
-        <div className="col-6">
-          <div className="nickname">{curNickname}</div>
-          {/* <i className="pi pi-cog" style={{ fontSize: "2rem" }}></i> */}
-          <Button
-            className="custom-button"
-            icon="pi pi-cog"
-            onClick={handleUpdateModal}
-          ></Button>
-          <MemberUpdateModal
-            showModal={showModal}
-            closeModal={handleUpdateModal}
-          />
+        <div className="col-7 profile-right">
+          <PointModal />
+          <DataTable value={infos}>
+            <Column field="my-team" header="My 팀"></Column>
+            <Column field="my-point" header="포인트"></Column>
+            <Column field="card-quantity" header="보유 카드"></Column>
+            <Column field="win-rate" header="베팅 승률"></Column>
+          </DataTable>
         </div>
       </div>
-      <div className="col-7 profile-right">
-        <DataTable value={infos}>
-          <Column field="my-team" header="My 팀"></Column>
-          <Column field="my-point" header="포인트"></Column>
-          <Column field="card-quantity" header="보유 카드"></Column>
-          <Column field="win-rate" header="베팅 승률"></Column>
-        </DataTable>
-      </div>
-
-      <PointModal />
     </div>
   );
 };
