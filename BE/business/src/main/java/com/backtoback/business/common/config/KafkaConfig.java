97742a5c1fe1c66b1c48952c47e674956782e463
KangHyojin1401 <khj6354@naver.com>
2023-05-19 10:06:50 +0900

    feat: 경기 결과 크롤링 후 이벤트 발행

--- BE/business/src/main/java/com/backtoback/business/common/config/KafkaConfig.java
+++ BE/business/src/main/java/com/backtoback/business/common/config/KafkaConfig.java
@@ -2,17 +2,25 @@ package com.backtoback.business.common.config;
 
 import java.util.function.Consumer;
 
+import org.springframework.cloud.stream.function.StreamBridge;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 import com.backtoback.business.common.dto.GameConditionDto;
+import com.backtoback.business.common.dto.GameResultKafkaRes;
 import com.backtoback.business.game.domain.GameActiveType;
+import com.backtoback.business.game.service.GameService;
 
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 
 @Configuration
 @Slf4j
+@RequiredArgsConstructor
 public class KafkaConfig {
+
+	private final StreamBridge streamBridge;
+	private final GameService gameService;
 
 	@Bean
 	// @Async
@@ -25,6 +33,8 @@ public class KafkaConfig {
 
 			if (gameActiveType == GameActiveType.AFTER_GAME) { //경기 끝
 				//경기 결과 크롤링
+				gameService.afterGameResult(gameSeq);
+				streamBridge.send("game-result-out-0", new GameResultKafkaRes(gameSeq));
 			}
 		};
 	}
---
+++ BE/business/src/main/java/com/backtoback/business/common/dto/GameResultKafkaRes.java
@@ -0,0 +1,16 @@
+package com.backtoback.business.common.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class GameResultKafkaRes {
+
+	private Long gameSeq;
+
+}
